2306cc2b605e6f7f3ae5aa58e6be434626f81dde
Luan de Oliveira <luan012012@hotmail.com>
2024-07-05 22:24:54 -0300

    feat: Segregando módulo principal

---
+++ leats/src/main/java/com/luandeoliveira/leats/LeatsApplication.java
@@ -0,0 +1,13 @@
+package com.luandeoliveira.leats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeatsApplication.class, args);
+	}
+
+}
---
+++ leats/src/test/java/com/luandeoliveira/leats/LeatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luandeoliveira.leats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
