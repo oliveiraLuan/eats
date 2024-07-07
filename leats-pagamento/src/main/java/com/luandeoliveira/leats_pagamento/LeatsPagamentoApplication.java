afd9dd301be16e6bcd730a34147b0821d891332c
Luan de Oliveira <luan012012@hotmail.com>
2024-07-06 22:59:56 -0300

    feat: adicionado microservice de pagamento

---
+++ leats-pagamento/src/main/java/com/luandeoliveira/leats_pagamento/LeatsPagamentoApplication.java
@@ -0,0 +1,13 @@
+package com.luandeoliveira.leats_pagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeatsPagamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeatsPagamentoApplication.class, args);
+	}
+
+}
---
+++ leats-pagamento/src/test/java/com/luandeoliveira/leats_pagamento/LeatsPagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luandeoliveira.leats_pagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeatsPagamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
